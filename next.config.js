d82aff26641ee1963bc82995f2aafbf24f866125
Calcium0920 <zepyx0920@gmail.com>
2025-08-06 01:45:45 +0900

    🎉 AI Marketplace Pro 完成！全機能実装完了

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
